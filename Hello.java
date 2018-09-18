140baf02ca861815b5be88ce0a15b68e42cdcd9f
严俊东 <1172482914@qq.com>
2018-09-18 21:35:45 +0800

    Hello World

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+// public 表示这个类是公共的，一个java文件中只能有一个public类
+// class 表示这是一个类
+// Hello 类名（公共类的类名必须和文件名一致）
+public class Hello {
+    // 一个主函数，相当于是程序的入口
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
